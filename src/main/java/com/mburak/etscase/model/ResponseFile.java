e605def13a2baf316b60f89f23c8d2dcb4e1800f
Mustafa Burak Orhan <borhan96@hotmail.com>
2023-02-26 16:39:48 +0300

    Created the Controller Layer with methods according to file manage operations and upload a file with all of attributes to database #8 #10

---
+++ src/main/java/com/mburak/etscase/model/ResponseFile.java
@@ -0,0 +1,50 @@
+package com.mburak.etscase.model;
+
+public class ResponseFile {
+    private long id;
+    private String fileName;
+    private String filePath;
+    private String fileType;
+    private Long fileSize;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileExtension) {
+        this.fileType = fileType;
+    }
+
+    public Long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(Long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+}
